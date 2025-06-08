466784c8e77d9cf2f7bab03d05361ca668a41ba7
Kuchumova_Ekaterina <e_kuchumova_06@mail.ru>
2025-06-08 17:34:05 +0300

    Update train_1.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,65 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+#include <iostream>
+Train::Train() : countOp(0), first(nullptr) {}
+
+void Train::addCar(bool light) {
+    Car* carr = new Car{ light, nullptr, nullptr };
+    if (!first) {
+        carr->next = carr;
+        carr->prev = carr;
+        first = carr;
+    }
+    else {
+        Car* last = first->prev;
+        carr->next = first;
+        carr->prev = last;
+        last->next = carr;
+        first->prev = carr;
+    }
+}
+
+int Train::getLength() {
+
+    if (!first) return 0;
+
+    countOp = 0;
+    Car* currentt = first;
+
+    // Включаем лампу в начальном вагоне
+    currentt->light = true;
+
+    int maxSteps = 1000000; // защита от бесконечного цикла
+
+    while (maxSteps-- > 0) {
+        int stepss = 0;
+        Car* walkerr = currentt->next;
+        countOp++;
+
+        // Идём вперёд до включённой лампочки
+        while (!walkerr->light) {
+            stepss++;
+            walkerr = walkerr->next;
+            countOp++;
+        }
+
+        // Вернуться назад на steps шагов
+        Car* checkerr = walkerr;
+        for (int i = 0; i < stepss; ++i) {
+            checkerr = checkerr->prev;
+            countOp++;
+        }
+
+        // если лампочка погасла обошли полный круг длина найдена
+        if (!checkerr->light)
+            return stepss + 1;
+        // иначе — продолжаем обход
+        else
+            currentt = currentt->next;
+    }
+    std::cerr << "Ошибка: превышен лимит шагов. Возможен бесконечный цикл.\n";
+    return -1; // ошибка
+}
+int Train::getOpCount() {
+    return countOp;
+}
